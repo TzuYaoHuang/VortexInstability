26dafc66d0d282ffe538ec61ccfc493053f3ae42
Tzu-Yao Huang <tzuyao.jason.huang@gmail.com>
2026-05-09 15:34:23 +0200

    include another regime map

--- src/qVortex_regimeMap.jl
+++ src/qVortex_regimeMap.jl
@@ -3,23 +3,43 @@ cd(@__DIR__)
 
 include("util.jl")
 
-qList = -1.5:0.15:1.5
-ρList = 10 .^ (-1:0.1:1)
-log₁₀ρ = log10.(ρList)
-α_test = 1.4
-n_test = -1
-rv_test = 1 #1.12
+# qList = -1.5:0.15:1.5
+# ρList = 10 .^ (-1:0.2:1)
+# log₁₀ρ = log10.(ρList)
+# α_test = 0.35
+# n_test = -1
+# rv_test = 1.12 #1.12
 
-σMat = zeros((length(qList), length(ρList)))
+# σMat = zeros((length(qList), length(ρList)))
 
-for iq∈eachindex(qList), iρ∈eachindex(ρList)
-    q_test = qList[iq]
-    λρ_test = ρList[iρ]
+# for iq∈eachindex(qList), iρ∈eachindex(ρList)
+#     q_test = qList[iq]
+#     λρ_test = ρList[iρ]
+#     r_grid, best_val, F_mode, G_mode, H_mode, P_mode, ζ₀, all_sigmas, all_vecs, sort_idx = solve_inviscid_multiphase_qvortex(α_test, n_test, q_test, rv_test, 1, λρ_test, N1=50, N2=50)
+#     σMat[iq, iρ] = imag(best_val)
+# end
+
+# plot(size=(700,500))
+# contourf!(qList, log₁₀ρ, σMat', xlimit=(-1.5,1.5), ylimit=(-1,1), aspect_ratio=:equal, color=:PuBuGn)
+# plot!(xlabel="q", ylabel="log10(ρout/ρin)", title="α=$α_test, n=$n_test, rv=$rv_test")
+
+q_test = 0
+λρ_test = 1
+# log₁₀ρ = log10.(ρList)
+αList = 0.01:0.1:1.5
+nList = -6:6
+rv_test = 1.12 #1.12
+
+σMat = zeros((length(αList), length(nList)))
+
+for ik∈eachindex(αList), im∈eachindex(nList)
+    α_test = αList[ik]
+    n_test = nList[im]
     r_grid, best_val, F_mode, G_mode, H_mode, P_mode, ζ₀, all_sigmas, all_vecs, sort_idx = solve_inviscid_multiphase_qvortex(α_test, n_test, q_test, rv_test, 1, λρ_test, N1=50, N2=50)
-    σMat[iq, iρ] = imag(best_val)
+    σMat[ik, im] = imag(best_val)
 end
 
 plot(size=(700,500))
-contourf!(qList, log₁₀ρ, σMat', xlimit=(-1.5,1.5), ylimit=(-1,1), aspect_ratio=:equal, color=:PuBuGn)
-plot!(xlabel="q", ylabel="log10(ρout/ρin)", title="α=$α_test, n=$n_test, rv=$rv_test")
+contourf!(αList, nList, σMat', xlimit=(0,1.5), ylimit=(-6,6), color=:PuBuGn)
+plot!(xlabel="α", ylabel="n", title="q=$q_test, λρ=$λρ_test, rv=$rv_test")
 
