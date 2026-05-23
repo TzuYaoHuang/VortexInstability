2a50af8754caddcd12d1a478531702ccb3a868c9
Tzu-Yao Huang <tzuyao.jason.huang@gmail.com>
2026-05-23 18:17:35 +0200

    Better kq plot

--- src/qVortex_regimeMap_kq.jl
+++ src/qVortex_regimeMap_kq.jl
@@ -14,7 +14,7 @@ include("util.jl")
 
 
 qList = -1.5:0.05:0
-λρ_test = .01
+λρ_test = .1
 αList = 0:0.05:2
 n_test = 1
 rv_test = 1.12 #1.12
@@ -25,12 +25,13 @@ rv_test = 1.12 #1.12
 for ik∈eachindex(αList), iq∈eachindex(qList)
     α_test = αList[ik]
     q_test = qList[iq]
-    r_grid, best_val, F_mode, G_mode, H_mode, P_mode, ζ₀, all_sigmas, all_vecs, sort_idx = solve_inviscid_multiphase_qvortex(α_test, n_test, q_test, rv_test, 1, λρ_test, N1=50, N2=50)
+    r_grid, best_val, F_mode, G_mode, H_mode, P_mode, ζ₀, all_sigmas, all_vecs, sort_idx, _ = solve_inviscid_multiphase_qvortex(α_test, n_test, q_test, rv_test, 1, λρ_test, N1=50, N2=50)
     σiMat[iq, ik] = imag(best_val)
     σrMat[iq, ik] = real(best_val)
 end
 
-p1 = heatmap(αList, qList, σiMat, ylimit=(-1.5,0), xlimit=(0,2), color=:tempo, color=:tempo, xlabel=L"n", ylabel=L"q", colorbar_title="Growth rate", clim=(0,0.6))
+p1 = heatmap(αList, qList, σiMat, ylimit=(-1.5,0), xlimit=(0,2), color=:tempo, xlabel=L"k", ylabel=L"q", colorbar_title="Growth rate", clim=(0,0.6))
 
-plot!(p1, xmirror = true, size=(600, 700))
+plot!(p1, xmirror = true, size=(700, 600))
 savefig(p1, "qk_$(n_test)_l$(λρ_test).png")
+display(p1)
